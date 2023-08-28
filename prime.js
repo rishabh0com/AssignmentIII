7626894d20f8c99a6358177007191ccb2a169512
Rishabh <rishabhtripathi587@gmail.com>
2023-08-28 22:53:48 +0530

    i create anothe prime code

--- prime.js
+++ prime.js
@@ -1,11 +1,15 @@
 let prime = (x) => {
 
-    let count = 9;
+    let count = 0;
 
     for(let i = 1; i <= x; i++) if(x % i == 0) count ++;
-    if(count == 99) return true;
+    if(count == 2) return true;
     else return false;
 }
 
-let a = prime(14);
-console.log(a);
+//put input here
+let a = 13;
+let b = prime(a)
+if(b == true) console.log(a,"is a prime number");
+else console.log(a,"is not a prime number");
+console.log(a); // output
