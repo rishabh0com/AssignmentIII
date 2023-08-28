3c740961404106202fc66ae2f549cb85c8e2b23d
Mihir Soni <ms6711534@gmail.com>
2023-08-28 22:48:05 +0530

    I have updated the codes

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 let prime = (x) => {
 
-    let count = 9;
+    let count = 0;
 
     for(let i = 1; i <= x; i++) if(x % i == 0) count ++;
-    if(count == 99) return true;
+    if(count == 2) return true;
     else return false;
 }
 
-let a = prime(14);
-console.log(a);
+let a = 23;
+if(prime(a)) console.log("Prime");
